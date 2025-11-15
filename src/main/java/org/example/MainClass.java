aa6bb053d06693dd3529bb0fd18f098500ff2cb5
altyna <altyna.behtenova@gmail.com>
2025-11-15 15:07:07 +0700

    Add testGetClassString

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class MainClass {
     private int class_number = 20;
+    private String class_string = "Hello, world";
+
+    public String getClassString() {
+        return class_string;
+    }
 
     public int getClassNumber() {
         return class_number;
--- src/main/java/org/example/MainClassTest.java
+++ src/main/java/org/example/MainClassTest.java
@@ -3,7 +3,13 @@ package org.example;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass{
+public class MainClassTest extends MainClass {
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("Фраза не содержит \"Hello\" или \"hello\"",
+                getClassString().contains("Hello") || getClassString().contains("hello"));
+    }
+
     @Test
     public void testGetClassNumber() {
         Assert.assertTrue("Число меньше или равно 45", getClassNumber() > 45);

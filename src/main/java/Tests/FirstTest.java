d5740fa74638275852d3e002b5c15c7a1757d0fc
altyna <altyna.behtenova@gmail.com>
2025-11-24 21:40:21 +0700

    Add FirstTest

---
+++ src/main/java/Tests/FirstTest.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appium:deviceName", "AndroidTestDevice");
+        capabilities.setCapability("appium:platformVersion", "16.0");
+        capabilities.setCapability("appium:appPackage", "org.wikipedia");
+        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appium:app", "C:\\Users\\altyn\\OneDrive\\Рабочий стол\\JavaAppiumAutomation\\JavaAppiumAutomation\\apks\\org.wikipedia_50550.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void FirstTest() {
+        System.out.println("First test run");
+    }
+}
